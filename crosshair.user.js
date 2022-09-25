8adc834cf5dd2d4b82ff054ac3f5a7011b969da4
Cassandra-uwu <114366158+Cassandra-uwu@users.noreply.github.com>
2022-09-25 18:13:07 -0400

    Add files via upload

---
+++ crosshair.user.js
@@ -0,0 +1,29 @@
+// ==UserScript==
+// @name         crosshair
+// @namespace    https://cassandra.lol
+// @version      0.1
+// @description  cubeshot crosshair
+// @author       cassandra
+// @match        *cubeshot.io/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cubeshot.io
+// @grant        none
+// ==/UserScript==
+
+var newHTML         = document.createElement ('div');
+newHTML.innerHTML   = '             \            \
+        <img id="crosshair" src="https://cdn.discordapp.com/attachments/516664193668218912/918429080955797514/asdasdasdasdgwewefa.png" </img>       \                        \
+';
+function addGlobalStyle(css) {
+    var head, style;
+    head = document.getElementsByTagName('head')[0];
+    if (!head) { return; }
+    style = document.createElement('style');
+    style.type = 'text/css';
+    style.innerHTML = css;
+    head.appendChild(style);
+}
+
+addGlobalStyle('#crosshair { position:absolute;display:block;left:950px;top:530px; }');
+
+addGlobalStyle('body { color: white; background-color: black; }');
+document.body.appendChild (newHTML);
